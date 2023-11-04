8f96be77156c0228138e8574d98042df7520ec05
barrosdaniel <barros.daniel@outlook.com>
2023-11-04 15:27:12 +0800

    feature: Complete Revenue.

--- script.py
+++ script.py
@@ -12,7 +12,22 @@ number_of_prices = len(prices)
 
 average_price = total_price / number_of_prices
 
-print(f"Avergage Haircut Price: \n{average_price:.2f}")
+print(f"Avergage Haircut Price: $ {average_price:.2f}")
 
+# 2) Calculate new prices
 new_prices = [price - 5 for price in prices]
 print(new_prices)
+
+# 3) Calculate revenue
+total_revenue = 0
+for i in range(len(hairstyles)):
+    total_revenue += prices[i] * last_week[i]
+print(f"Total Revenue: $ {total_revenue:.2f}")
+
+# 4) Calculate average daily revenue
+average_daily_revenue = total_revenue / 7
+print(f"Average Daily Revenue: $ {average_daily_revenue:.2f}")
+
+# 5) List of cuts under $30
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+print(cuts_under_30)
